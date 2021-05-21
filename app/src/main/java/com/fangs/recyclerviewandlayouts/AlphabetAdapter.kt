c18b1206fdc23bd8bac6284e2d66616fa456f196
Fangszx <jygrzn@gmail.com>
2021-05-21 20:16:28 +0800

    add second recycler view (alphabet)

---
+++ app/src/main/java/com/fangs/recyclerviewandlayouts/AlphabetAdapter.kt
@@ -0,0 +1,5 @@
+package com.fangs.recyclerviewandlayouts
+
+class AlphabetAdapter(mainActivity: MainActivity, i: Int) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+}
--- app/src/main/java/com/fangs/recyclerviewandlayouts/MainActivity.kt
+++ app/src/main/java/com/fangs/recyclerviewandlayouts/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.fangs.recyclerviewandlayouts
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -11,11 +10,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //numbers
+        //region
         val rvNumbers = findViewById<RecyclerView>(R.id.rv_countingNumbers)
         //scaffold
         rvNumbers.layoutManager = GridLayoutManager(this, 5)
         rvNumbers.setHasFixedSize(true)
         rvNumbers.adapter = NumbersAdapter(this,10)
+        //endregion
+
+        //alphabets
+        //region
+        val rvAlphabet = findViewById<RecyclerView>(R.id.rv_alphabets)
+        rvAlphabet.layoutManager = GridLayoutManager(this, 5)
+        rvAlphabet.setHasFixedSize(true)
+        rvAlphabet.adapter = AlphabetAdapter(this, 20)
+
+
+        //endregion
 
 
 
