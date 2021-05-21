a2a06126b39cc36f56a369ab1d24342996fdc6a3
Fangszx <jygrzn@gmail.com>
2021-05-21 20:20:19 +0800

    edit alphabet card

--- app/src/main/java/com/fangs/recyclerviewandlayouts/AlphabetAdapter.kt
+++ app/src/main/java/com/fangs/recyclerviewandlayouts/AlphabetAdapter.kt
@@ -1,5 +1,27 @@
 package com.fangs.recyclerviewandlayouts
 
-class AlphabetAdapter(mainActivity: MainActivity, i: Int) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class AlphabetAdapter(private val context: Context, private val alphabetsLength: Int) :
+    RecyclerView.Adapter<AlphabetAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        LayoutInflater.from(context).inflate(R.layout.alphabet_card, parent, false)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int = alphabetsLength
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
 
 }
